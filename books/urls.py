6b37c8bf71719616b07006b8932eae962c0718b5
Maria Maksymenko <136785612+Maksikd62@users.noreply.github.com>
2024-12-18 17:43:13 +0200

    layout and delete

--- books/urls.py
+++ books/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from books import views
 
 urlpatterns = [
-    path("list/", views.list),
-    path("detail/<int:id>", views.detail),
+    path("list/", views.list, name='list'),
+    path("detail/<int:id>", views.detail, name='detail'),
+    path("delete/<int:id>", views.delete, name='delete'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 
 from books.models import Book
@@ -11,5 +11,13 @@ def detail(request, id):
     try:
         book = Book.objects.get(id=id)
         return render(request, "detail.html", {"book": book})
+    except Book.DoesNotExist:
+        return HttpResponse("Book not found", status=404)
+    
+def delete(request, id):
+    try:
+        book = Book.objects.get(id=id)
+        book.delete()
+        return redirect("/books/list")
     except Book.DoesNotExist:
         return HttpResponse("Book not found", status=404)
